bc7a92d5eb53b4f4c403d89a10d1489cc001d91d
otofune <otofune@otofune.me>
2021-11-19 16:29:26 +0900

    Use webextension-polyfill

--- src/content.ts
+++ src/content.ts
@@ -1,12 +1,8 @@
 // for reading "window.pantheon_account_chooser_data" (global variable), need to exit from isolated environment
 // see: https://developer.chrome.com/docs/extensions/mv3/content_scripts/#isolated_world
+
+import browser from "webextension-polyfill"
 const path = "/src/inject.js";
-try {
-  const script = document.createElement("script");
-  script.src = browser.runtime.getURL(path);
-  document.head.append(script);
-} catch (e) {
-  const script = document.createElement("script");
-  script.src = chrome.runtime.getURL(path);
-  document.head.append(script);
-}
+const script = document.createElement("script");
+script.src = browser.runtime.getURL(path);
+document.head.append(script);
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,6 +1,3 @@
 interface Window {
   pantheon_account_chooser_data: unknown;
 }
-
-declare var browser; // FIXME
-declare var chrome; // FIXME
